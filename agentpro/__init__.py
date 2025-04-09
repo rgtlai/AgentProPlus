42588c72ae1b51530d886132946a3bfa3863bafb
ram-traversaal <ram@traversaal.ai>
2025-04-09 13:00:25 -0400

    Update __init__.py

--- agentpro/__init__.py
+++ agentpro/__init__.py
@@ -1,8 +1,8 @@
 from .agent import AgentPro
 from typing import Any
-from agentpro.tools import AresInternetTool, CodeEngine, YouTubeSearchTool, SlideGenerationTool # add more tools when available
+from agentpro.tools import AresInternetTool, CodeEngine, YouTubeSearchTool#, SlideGenerationTool # add more tools when available
 ares_tool = AresInternetTool()
 code_tool = CodeEngine()
 youtube_tool = YouTubeSearchTool()
-slide_tool = SlideGenerationTool()
-__all__ = ['AgentPro', 'ares_tool', 'code_tool', 'youtube_tool', 'slide_tool'] # add more tools when available
+#slide_tool = SlideGenerationTool()
+__all__ = ['AgentPro', 'ares_tool', 'code_tool', 'youtube_tool']#, 'slide_tool'] # add more tools when available
